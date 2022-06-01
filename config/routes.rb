c5af0af18aeb3a7d89980ebe43135d66c92b824d
CARLOS-SSH <carlos.anezeh@gmail.com>
2022-05-31 20:04:57 -0500

    Create user authentication with devise gem and fixed issues.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'pages/index'
+  devise_for :employees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
