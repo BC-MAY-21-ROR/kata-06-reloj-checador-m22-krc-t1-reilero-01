b99ef6b1c4223398fdd52c2a3a779b0cc4bf49a4
ever <ever864@gmail.com>
2022-06-06 17:11:12 -0500

    Fixed routes for form partial

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/dashboard' => "dashboard#index", :as => :dashboard_root
   get '/employee_create' => "dashboard#new"
   post '/employee_create' => "dashboard#create"
-  get '/employee_edit/:id' => "dashboard#edit"
+  get '/employee_edit/:id' => "dashboard#edit", :as => :employee_edit
   post '/employee_edit/:id' => "dashboard#update"
   get '/employee_show/:id' => "dashboard#show"
 
