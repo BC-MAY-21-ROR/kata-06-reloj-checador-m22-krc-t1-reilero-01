29e7afabe6df9f000f1f81a9387459620b5103bd
EdwardMD <Edwardmi444@gmail.com>
2022-06-06 16:56:59 -0500

    Creation of store model and routing of the data

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,39 @@
+class StoreController < ApplicationController
+    def index
+        @stores = Store.all
+    end
+
+    def show
+        @store = Store.find(params[:id])
+    end
+
+    def edit
+        @store = Store.find(params[:id])
+    end
+
+    def update
+        @store = Store.find(params[:id])
+        if @store.update(store_params)
+            redirect_to @store
+        else
+            render 'edit'
+        end
+    end
+
+    def new
+        @store = Store.new
+    end
+
+    def create
+        @store = Store.new(store_params)
+        if @store.save
+            redirect_to dashboard_index_path
+        else
+            render :new
+        end
+    end
+
+    def store_params
+        params.permit(:id, :name, :address)
+    end
+end
