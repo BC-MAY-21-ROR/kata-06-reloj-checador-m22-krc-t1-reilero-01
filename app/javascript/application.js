9e654acbb22218cec82e41ae9baa7b47a86930f2
ever <ever864@gmail.com>
2022-06-22 13:38:06 -0500

    Refactoring turbo rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
 import "./custom/clock_live"
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
